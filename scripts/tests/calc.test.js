56de0dfaaa8a8859e0d3c90e21a69ae5bb164803
lukaszkukla <lukaszkukla@gmail.com>
2022-05-19 21:14:01 +0000

    1st mock test

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -1,9 +1,22 @@
+/**
+ * @jest-environment jsdom
+ */
+
 const addition = require('../calc');
 
 describe('Calculator', () => {
     describe('Addition function', () => {
         test('should return 42 for 20 + 22', () => {
             expect(addition(20, 22)).toBe(42);
+        })
+        test('should return 43 for 21 + 22', () => {
+            expect(addition(21, 22)).toBe(43);
+        })
+        test('should return 73 for 50 + 23', () => {
+            expect(addition(50, 23)).toBe(73);
+        })
+        test('should return 81 for 46 + 35', () => {
+            expect(addition(46, 35)).toBe(81);
         })
     });
     describe('Subtraction function', () => {
